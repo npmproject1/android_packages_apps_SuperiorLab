5eb55c83f3bf27234a9d640b88eebf6f4b4ef99f
Idc <59904122+IDontCare-05@users.noreply.github.com>
2023-06-08 09:47:22 +0530

    Add volume Style Customization[2/3]

--- src/com/superior/lab/fragments/ButtonSettings.java
+++ src/com/superior/lab/fragments/ButtonSettings.java
@@ -18,10 +18,15 @@
 
 package com.superior.lab.fragments;
 
+import static android.os.UserHandle.USER_CURRENT;
+
 import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.content.om.IOverlayManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.os.Vibrator;
 import androidx.preference.PreferenceCategory;
@@ -35,11 +40,24 @@ import android.provider.Settings;
 import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.superior.support.preferences.SystemSettingListPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 
 public class ButtonSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener{
+        
+       private static final String VOLUMEBAR_STYLES = "VOLUME_BAR_STYLES";
+
+    private static final String VOLUMEBAR_OVERLAY_STYLE1 = "com.custom.overlay.systemui.volume1";
+    private static final String VOLUMEBAR_OVERLAY_STYLE2 = "com.custom.overlay.systemui.volume2"; 
+    private static final String VOLUMEBAR_OVERLAY_STYLE3 = "com.custom.overlay.systemui.volume3";        
+    private static final String VOLUMEBAR_OVERLAY_STYLE4 = "com.custom.overlay.systemui.volume4"; 
+    private static final String VOLUMEBAR_OVERLAY_STYLE5 = "com.custom.overlay.systemui.volume5";
+    
+    private SystemSettingListPreference CustomVolumeStyle;
+    private IOverlayManager mOverlayService; 
+    private Context mContext;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -47,12 +65,78 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.superior_lab_button);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
+        mContext = getActivity();
+        final PreferenceScreen screen = getPreferenceScreen();
+
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+                
+        CustomVolumeStyle = (SystemSettingListPreference) findPreference("VOLUME_BAR_STYLES");
+        int CuVolumeStyle = Settings.System.getIntForUser(getContentResolver(),
+                "VOLUME_BAR_STYLES", 0, UserHandle.USER_CURRENT);
+        int valueIndexvol = CustomVolumeStyle.findIndexOfValue(String.valueOf(CuVolumeStyle));
+        CustomVolumeStyle.setValueIndex(valueIndexvol >= 0 ? valueIndexvol : 0);
+        CustomVolumeStyle.setSummary(CustomVolumeStyle.getEntry());
+        CustomVolumeStyle.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-
+        
+        if (preference == CustomVolumeStyle) {
+            int VolumeStyle = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    "VOLUME_BAR_STYLES", VolumeStyle, UserHandle.USER_CURRENT);
+            CustomVolumeStyle.setSummary(CustomVolumeStyle.getEntries()[VolumeStyle]);
+                if (VolumeStyle == 0) {
+                   try {
+                      mOverlayService.setEnabled(VOLUMEBAR_OVERLAY_STYLE1, false, USER_CURRENT);
+                      mOverlayService.setEnabled(VOLUMEBAR_OVERLAY_STYLE2, false, USER_CURRENT);
+                      mOverlayService.setEnabled(VOLUMEBAR_OVERLAY_STYLE3, false, USER_CURRENT);
+                      mOverlayService.setEnabled(VOLUMEBAR_OVERLAY_STYLE4, false, USER_CURRENT);
+                      mOverlayService.setEnabled(VOLUMEBAR_OVERLAY_STYLE5, false, USER_CURRENT);     
+                   } catch (RemoteException re) {
+                      throw re.rethrowFromSystemServer();
+                   }
+               } else if (VolumeStyle == 1) {
+                   try {
+                      mOverlayService.setEnabledExclusiveInCategory(VOLUMEBAR_OVERLAY_STYLE1, USER_CURRENT);   
+                   } catch (RemoteException re) {
+                      throw re.rethrowFromSystemServer();
+                   }
+               } else if (VolumeStyle == 2) {
+                   try {
+                      mOverlayService.setEnabledExclusiveInCategory(VOLUMEBAR_OVERLAY_STYLE2, USER_CURRENT);   
+                   } catch (RemoteException re) {
+                      throw re.rethrowFromSystemServer();
+                   }
+                } else if (VolumeStyle == 3) {
+                   try {
+                      mOverlayService.setEnabledExclusiveInCategory(VOLUMEBAR_OVERLAY_STYLE3, USER_CURRENT);     
+                   } catch (RemoteException re) {
+                      throw re.rethrowFromSystemServer();
+                   }
+                } else if (VolumeStyle == 4) {
+                   try {
+                      mOverlayService.setEnabledExclusiveInCategory(VOLUMEBAR_OVERLAY_STYLE4, USER_CURRENT);     
+                   } catch (RemoteException re) {
+                      throw re.rethrowFromSystemServer();
+                   }
+                } else if (VolumeStyle == 5) {
+                   try {
+                      mOverlayService.setEnabledExclusiveInCategory(VOLUMEBAR_OVERLAY_STYLE5, USER_CURRENT);     
+                   } catch (RemoteException re) {
+                      throw re.rethrowFromSystemServer();
+                   }
+                }    
+            return true;
+          }
         return false;
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
     @Override
